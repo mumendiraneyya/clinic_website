05e695deba02246f9a0f0ae579ef6f0e7b21c701
Orwa Diraneyya <info@orwa.tech>
2025-12-13 01:35:20 +0300

    fixed map and google-map link

--- src/navigation.ts
+++ src/navigation.ts
@@ -84,7 +84,7 @@ export const footerData = {
     { ariaLabel: 'Facebook', icon: 'tabler:brand-facebook', href: 'https://www.facebook.com/Abuobaydatajjarrah/' },
     { ariaLabel: 'Instagram', icon: 'tabler:brand-instagram', href: 'https://www.instagram.com/dr.mumen.diraneyya/' },
     { ariaLabel: 'YouTube', icon: 'tabler:brand-youtube', href: 'https://www.youtube.com/channel/UCe9xMNuVzNgEWvoui3nolRw' },
-    { ariaLabel: 'Google Business', icon: 'tabler:brand-google', href: 'https://share.google/Mxc9g3fsBrmQv7qHW' },
+    { ariaLabel: 'Google Business', icon: 'tabler:brand-google', href: 'https://maps.app.goo.gl/RE3yv34U16Ssdezj7' },
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/DrMumenDiraneya' },
     { ariaLabel: 'Tebcan', icon: 'brand-tebcan-alt', href: 'https://tebcan.com/ar/Jordan/dr/%D8%AF%D9%83%D8%AA%D9%88%D8%B1-%D9%85%D8%A4%D9%85%D9%86-%D9%85%D8%A3%D9%85%D9%88%D9%86-%D8%AF%D9%8A%D8%B1%D8%A7%D9%86%D9%8A%D8%A9-%D8%AC%D8%B1%D8%A7%D8%AD%D8%A9-%D8%B9%D8%A7%D9%85%D8%A9_2822' },
     { ariaLabel: 'RSS', icon: 'tabler:rss', href: getAsset('/rss.xml') },
